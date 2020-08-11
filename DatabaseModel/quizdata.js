86dad6450162d7345d2d481cf2533274575dc325
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-08-11 23:47:34 +0530

    add a database model schema

---
+++ DatabaseModel/quizdata.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const queSchema = new schema({
+    questionString :{
+        type:String,
+        required:true
+    },
+    option1 : {
+        type:String,
+        required:true
+    },
+    option2 : {
+        type:String,
+        required:true
+    },
+    option3 : {
+        type:String,
+        required:true
+    },
+    option4 : {
+        type:String,
+        required:true
+    },
+    correct : {
+        type:String,
+        required:true
+    }
+});
+const QuizSchema = new schema({
+    quizId: {
+        type: String,
+        required: true
+    },
+    questions: [queSchema]
+});
+module.exports={
+    queSchema : queSchema,
+    QuizSchema: QuizSchema
+};
