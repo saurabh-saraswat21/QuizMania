42b26b3852ae448270441307af3441aae4eb35c3
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-08-19 14:07:11 +0530

    add a new schema for only questions

--- DatabaseModel/quizdata.js
+++ DatabaseModel/quizdata.js
@@ -1,9 +1,37 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
+const onlyquestions = new schema({
+    questionString :{
+        type:String,
+        required:true
+    },
+    option1 : {
+        type:String,
+        required:true
+    },
+    option2 : {
+        type:String,
+        required:true
+    },
+    option3 : {
+        type:String,
+        required:true
+    },
+    option4 : {
+        type:String,
+        required:true
+    },
+    correct : {
+        type:String,
+        required:true
+    }
+    
+})
+
 const queSchema = new schema({
     quizId: {
-        type: String,
+        type: Number,
         required: true
     },
     questionString :{
@@ -33,10 +61,10 @@ const queSchema = new schema({
 });
 const QuizSchema = new schema({
     quizId: {
-        type: String,
+        type: Number,
         required: true
     },
-    questions: [queSchema]
+    questions: [onlyquestions]
 });
 module.exports={
     queSchema : queSchema,
