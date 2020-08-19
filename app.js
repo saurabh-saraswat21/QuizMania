9f92f9a660f2aad4f3e86294ef7603db48d75973
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-08-19 14:03:12 +0530

    formating

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require('mongoose');
-const indexcreateinsert =require('./controllers/indexcreateinsert');
+
 const app = express();
 const port = 80;
-app.use(express.static('public'))
+app.use(express.static('public'));
+
 app.use(express.urlencoded({ extended: true }));
+
+const indexcreateinsert =require('./controllers/indexcreateinsert');
 mongoose.connect('mongodb://localhost:27017/quizmania', { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+
 .then((result) => { app.listen(port, () => { console.log('Listening..') }) })
 .catch((err) => console.log(err));
 
@@ -16,6 +22,3 @@ mongoose.connection.once('open', () => {
 })
 
 indexcreateinsert(app);
-
- 
-
