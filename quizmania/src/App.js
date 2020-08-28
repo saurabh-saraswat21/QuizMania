2c3ef94c702cf9a9bd433a766e497e9c71bb120e
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-08-28 15:21:19 +0530

    add Routing

--- quizmania/src/App.js
+++ quizmania/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import Home from './Components/home'
+import Home from './Components/MainPagesComp/home'
 import Navbar from './Components/partials/navbar';
+import {BrowserRouter,Route} from 'react-router-dom'
+import createQuiz from './Components/HomePageComp/createQuiz';
+
 
 
 class App extends Component {
   render() {
 
     return (
+      <BrowserRouter>
       <div className="App">
         
+        {/* NavBar that is alwasy going to show at the top of the website */}
         <Navbar/>
 
 
+
+
+
+        {/* Routing for the different pages */}
+        <Route exact path ='/' component={Home}/>
+        <Route exact path ='/createquiz' component={createQuiz}/> 
+
       </div>
+      </BrowserRouter>
     );
   }
 }
