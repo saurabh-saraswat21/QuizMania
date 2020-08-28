e6beb03f32285258d69086c7d86b63793be39a97
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-08-28 16:26:53 +0530

    use Link instead of a tag

--- quizmania/src/Components/MainPagesComp/home.js
+++ quizmania/src/Components/MainPagesComp/home.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import {Link ,NavLink} from 'react-router-dom' 
+
 const Home = () => {
     return (
         <div className="homePage">
             <div className="btn container">
 
-                <a className="main btn" href="/createquiz"> Create Quiz</a>
-                <a className="main btn" href="/viewquizes"> View Quizes</a>
+                <div className="main btn"><Link to ="/createquiz" className="k">Create Quiz</Link> </div>
+                <div className="main btn"><Link to ="/viewquiz">View Quiz</Link> </div>
             </div>
         </div>
     )
