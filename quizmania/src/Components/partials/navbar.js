0c11c9257721ce275f5efe77c5b8b86d1647c5e2
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-08-28 14:08:00 +0530

    add a nav bar

---
+++ quizmania/src/Components/partials/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+const Navbar = () => {
+    return (
+
+        <div className="nav">
+            <nav className="navigation">
+                <ul className="navbar">
+                    <li className="logo"><a href="/">QuizMania</a></li>
+                    <li className="items main"><a href="#">Login</a></li>
+                    <li className="items main secondary"><a href="#">JoinQuiz</a></li>
+                    <li className="items"><a href="#">SignUp</a></li>
+                    <li className="items"><a href="/contact ">Contact</a></li>
+                    <li className="items"><a href=" # ">About us</a></li>
+                    <li className="toggle"><span className="hamburger"></span></li>
+
+                </ul>
+            </nav>
+
+        </div>
+    )
+}
+export default Navbar
