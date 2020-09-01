5c41007accb0c1bfe4020d1998200e69914f5586
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-01 17:07:49 +0530

    init server.js connect database and listen

---
+++ quizmania/backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { urlencoded } = require('body-parser');
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express();
+const port = 80;
+app.use(urlencoded({ extended: true }))
+app.use(cors());
+
+
+mongoose.connect('mongodb://localhost:27017/quizmania', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => { app.listen(port, () => { console.log('Listening..') }) })
+    .catch((err) => console.log(err));
+
+mongoose.connection.once('open', () => {
+    console.log('connection made');
+}).on('err', (err) => {
+    console.log(err);
+})
