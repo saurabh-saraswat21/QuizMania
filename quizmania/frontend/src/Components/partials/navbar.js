7e632d2231b74a6d2043bc4456b15b7db1b03c52
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-01 17:13:44 +0530

    add toggle function to toggle navbar

--- quizmania/frontend/src/Components/partials/navbar.js
+++ quizmania/frontend/src/Components/partials/navbar.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import '../../stylesheets/style.css'
 import {Link } from 'react-router-dom' 
 const Navbar = () => {
+    const [clas,toggle] = useState(false)
+    const togglefunc =()=>{
+        toggle(!clas) 
+    }
+
     return (
 
         <div className="nav">
             <nav className="navigation">
                 <ul className="navbar">
                     <li className="logo"><a href="/">QuizMania</a></li>
-                    <li className="items main"><Link to="#">Login</Link></li>
-                    <li className="items main secondary"><Link to="#">JoinQuiz</Link></li>
-                    <li className="items"><Link to="#">SignUp</Link></li>
-                    <li className="items"><Link to="#">Contact</Link></li>
-                    <li className="items"><Link to="#">About us</Link></li>
-                    <li className="toggle"><span className="hamburger"></span></li>
+                    <li className={clas?"active":" "+"items main"}><Link to="#">Login</Link></li>
+                    <li className={clas?"active":" "+"items main secondary"}><Link to="#">JoinQuiz</Link></li>
+                    <li className={clas?"active":" "+"items"}> <Link to="#">SignUp</Link></li>
+                    <li className={clas?"active":" "+"items"}> <Link to="#">Contact</Link></li>
+                    <li className={clas?"active":" "+"items"}> <Link to="#">About us</Link></li>
+                    <li onClick={togglefunc} className="toggle"><span className="hamburger"></span></li>
 
                 </ul>
             </nav>
