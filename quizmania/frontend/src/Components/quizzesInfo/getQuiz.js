906241242d7c3b01acbfda0cefda8c455b73ae30
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-09 17:29:10 +0530

    get data from store

--- quizmania/frontend/src/Components/quizzesInfo/getQuiz.js
+++ quizmania/frontend/src/Components/quizzesInfo/getQuiz.js
@@ -1,57 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
+import { connect } from 'react-redux'
+import '../../stylesheets/quiz.css'
 class Quiz extends Component {
 
-    componentDidMount = () => {
-        // getting the quiz id for which the quiz is to be matched
-        const { quiz_id } = this.props.match.params
-
-        // the   array that will store the status of the view more buttons if its pressed or not
-        const showbtn = [];
-
-        // requesting server to get details of the quiz
-        fetch("http://localhost:80/viewquizes/" + quiz_id)
-
-            // conversion to JSON
-            .then(res => res.json())
-
-            .then(res2 => {
-                // initial status for the showbtn if false and size is equal to the number of buttons
-                for (let i = 0; i < res2.questions.length; i++) {
-                    showbtn[i] = "false";
-
-                }
-
-                // setting state with the initial showbtn array  and the quiz questions fetched from server
-                this.setState({
-                    questions: [...res2.questions],
-                    showbtn: showbtn
-                })
-
-            })
-
-    }
 
     // Empty state structure
     state = {
-        questions: [
-            {
-                _id: "",
-                questionString: '',
-                option1: '',
-                option2: '',
-                option3: '',
-                option4: '',
-                correct: ''
-
-            }
-        ],
         showbtn: []
-
     }
-
-    // show method that takes the index of the button and toggle it by making the corresonding state value to "true"
+    
     show = (index) => {
 
         // store current staus array in a new arrray
@@ -70,14 +28,17 @@ class Quiz extends Component {
     }
 
     render() {
+        const questions = this.props.questions;
+
 
         // getting questions from the state and mapping one by one
-        const queslist = this.state.questions.map((question, index) => {
+        const queslist = questions.map((question, index) => {
+            console.log(index)
 
 
             return (
 
-                 
+
                 <div className="question" key={question._id}>
 
                     <h4>{question.questionString}</h4>
@@ -86,12 +47,12 @@ class Quiz extends Component {
                     <Link to="#">
 
                         {/* Calling function  passing the current index  */}
-                        <button  onClick={(e) => {
+                        <button onClick={(e) => {
                             this.show(index)
-                        }}  value="View more">viewmore</button>
+                        }} value="View more">viewmore</button>
                     </Link>
 
-                            {/* classname is decided by the showbtn status at that index */}
+                    {/* classname is decided by the showbtn status at that index */}
                     <div className={this.state.showbtn[index] ? "boxhidden" : "boxactive" + index}>
                         <li className="options">Option1:-{question.option1}</li>
                         <li className="options">Option2:-{question.option2}</li>
@@ -118,5 +79,10 @@ class Quiz extends Component {
 
     }
 }
+const MapStateToProps = (state) => {
+    return {
+        questions: state.questions
+    }
+}
 
-export default Quiz
+export default connect(MapStateToProps)(Quiz)
