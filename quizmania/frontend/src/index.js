214d66f5595c50650cef133d7748072174434f90
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-09 11:26:58 +0530

    create a redux store

--- quizmania/frontend/src/index.js
+++ quizmania/frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'
 import * as serviceWorker from './serviceWorker';
+
+const quizStore = createStore();
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store ={quizStore}></Provider>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
@@ -13,5 +18,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-
+ 
 serviceWorker.register();
