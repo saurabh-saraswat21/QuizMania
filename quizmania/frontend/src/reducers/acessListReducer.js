d265ceacf0ae79e941d086d0a6e57c0d566f996a
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-10 16:20:03 +0530

    handling success action

--- quizmania/frontend/src/reducers/acessListReducer.js
+++ quizmania/frontend/src/reducers/acessListReducer.js
@@ -1,19 +1,40 @@
 const initState ={
-    questions: [
+    quizzes: [
         {
             _id: "000",
-            questionString: 'sample question',
-            option1: 'sample option 1',
-            option2: 'sample option 2',
-            option3: 'sample option 3',
-            option4: 'sample option 4',
-            correct: 'sample correct '
+            questions:[
+            {
+
+                questionString: 'sample question',
+                option1: 'sample option 1',
+                option2: 'sample option 2',
+                option3: 'sample option 3',
+                option4: 'sample option 4',
+                correct: 'sample correct '
+            }
+
+            ]
 
         }
     ],
-     quiz_ids :[123,213,1234,48682]
+     quiz_ids :[]
 }
 const acessListReducer =(state=initState,action)=>{
+    if(action.type === 'GET_QUIZ_DATA_SUCCESS')
+    {
+    var quiz_ids =[];
+    
+    for (let i = 0; i < action.data.length; i++) {
+         quiz_ids[i] = action.data[i].quiz_id;
+        
+    }
+      return{
+          quizzes:action.data,
+          quiz_ids : quiz_ids
+      }
+      
+    }
+    
 
     return state;
 }
