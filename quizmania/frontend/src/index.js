884cdd04278635a3f7dff516ace651ff77c29ccc
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-11 23:05:15 +0530

    adding comments

--- quizmania/frontend/src/index.js
+++ quizmania/frontend/src/index.js
@@ -8,11 +8,21 @@ import * as serviceWorker from './serviceWorker';
 import acessListReducer from './reducers/acessListReducer'
 import {fetchData} from './Actions/getQuizData'
 
+//creating  a redux store
 const quizStore = createStore(
+
+  //reducer of the store
     acessListReducer,
+
+    // middleware for Async request 
     applyMiddleware(thunk)
+
   );
+
+  //dispatching action from the action creator to fetch data initialy when the websites loads and store in the redux state
   quizStore.dispatch(fetchData())
+
+//subscribe to changes whenever that state of the store updates the subsribbe functin is going to execute 
   quizStore.subscribe(()=>{
     console.log("update")
 
@@ -20,8 +30,17 @@ const quizStore = createStore(
 
 ReactDOM.render(
   <React.StrictMode>
+
+    {/* Provider allows to connect redux store to the main App 
+    App and store are both wrapped up inside provider tag*/}
     <Provider 
-    store ={quizStore}><App />
+
+    // Attaching the store
+    store ={quizStore}>
+
+      {/* wrapping the app */}
+      <App />
+
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
