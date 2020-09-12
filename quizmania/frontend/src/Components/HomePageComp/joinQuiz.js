ced41604a2f1a8213616a2749d782a1b4ce36700
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-12 21:06:02 +0530

    create a basic joinQuiz component

--- quizmania/frontend/src/Components/HomePageComp/joinQuiz.js
+++ quizmania/frontend/src/Components/HomePageComp/joinQuiz.js
@@ -1,11 +1,67 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 
-function joinQuiz() {
-    return (
-        <div>
+
+const joinQuiz=(props)=> {
+    // getting quiz id list from props
+    const id_list =props.quiz_ids
+
+    // checking if list is empty
+    const Quiz_List = id_list.length ?
+
+    // if not empty
+    (
+        // mapping quiz one by one
+        id_list.map(quiz_id =>{
+
+            // retutrning some JSX
+            return(
+                
+                
+                                    // {/* //  a unique key that is quiz id itself */}
+                <div className="quiz" key = {quiz_id}>
+
+                    <img src="#" alt="quiz logo" className="quizimage" />
+                    
+                    {/* link every quiz id to view all the questions of the quiz */}
+                    <Link to ={{
+                        pathname :'/startQuiz/'+quiz_id,
+                        
+                    }} >{quiz_id}</Link>
+                   
+               
+                    
+                
+                
+                </div>
+            )
+        })
+    )
+    // if Empty 
+    :(
+        <h1>No quizzes</h1>
+    )
+
+    //main return component
+    return(
+        <div className="quizList">
+            {Quiz_List}
             
         </div>
     )
+
+   
 }
 
-export default joinQuiz
+//mapping state to props of component
+const MapStateToProps=(state)=>{
+    return{
+
+        //quiz_ids will be available in the props of the component
+        quiz_ids : state.quiz_ids 
+    }
+}
+
+// wrapping the component with the HOC
+export default connect(MapStateToProps)(joinQuiz)
