77dd4fc478be6b4e43e4eb30ee5543473486fa94
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-15 23:16:03 +0530

    import required files and packages

---
+++ quizmania/frontend/src/Components/JoinQuizComp/quizOngoing.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+// is used to acess the passed state in the link tag in the instruction page 
+import { withRouter } from "react-router";
+
+// importing the error component  
+import Directacess from '../errComponents/DirectAccess'
+
+import '../../stylesheets/quiz.css'
