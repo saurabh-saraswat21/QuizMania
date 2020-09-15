66e4aaa7d3c9c16778bc289e699ad6e26a73a2ff
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-15 22:51:24 +0530

    instruction and  info component of the quiz

---
+++ quizmania/frontend/src/Components/JoinQuizComp/startQuizHome.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import '../../stylesheets/startQuizHome.css'
+import { connect } from 'react-redux'
+import{Link} from 'react-router-dom'
+
+export class startQuizHome extends Component {
+
+
+    render() {
