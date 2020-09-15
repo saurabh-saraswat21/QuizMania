24752bc739f8097e5720835ab8bf8b8c4f25a992
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-09-15 22:52:27 +0530

    a function to find the quiz from the redux store

--- quizmania/frontend/src/Components/JoinQuizComp/startQuizHome.js
+++ quizmania/frontend/src/Components/JoinQuizComp/startQuizHome.js
@@ -7,3 +7,20 @@ export class startQuizHome extends Component {
 
 
     render() {
+// a  function that will map the quiz to the props before it renders for the first time
+const mapStateToProps = (state, defaultProps) => {
+
+    //defaultProps are the basic props of the component that can give access to route params 
+    // so that we know which quiz to fetch
+
+    // getting  id from router params and cnverting to int so that is is able to be compared 
+    const id = parseInt(defaultProps.match.params.quiz_id)
+
+    //returning the particular quiz that is to be viewed
+    return {
+
+        // find method iterates every quiz in the quizzes and return that quiz whose id matches the id we get from the params above
+        quiz: state.quizzes.find(quiz => quiz.quiz_id === id),
+
+    }
+}
