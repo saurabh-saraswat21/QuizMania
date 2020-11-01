2f85dfa780daa7fece06e1544f5ed8c461eeb148
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-11-01 22:44:52 +0530

    require socket

--- quizmania/backend/routers/startQuiz.js
+++ quizmania/backend/routers/startQuiz.js
@@ -2,3 +2,8 @@
     const users_model = require("../DatabaseModel/users")
     const usermodel = mongoose.model('user', users_model.usersListSchema);
     const bodyParser = require('body-parser');
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json());
+
+    const socket = require('socket.io');
+    const io = socket(server);
