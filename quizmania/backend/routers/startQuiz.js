b05481865b798a6396fe4c911c27f0a1427ec457
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-11-01 23:09:58 +0530

    socket on disconnect

--- quizmania/backend/routers/startQuiz.js
+++ quizmania/backend/routers/startQuiz.js
@@ -10,5 +10,51 @@
 
     io.on('connection', socket => {
         console.log("new socket connection");
+        socket.on('update_socket', (data) => {
+            socket.username = data.username;
+            socket.quiz_id = data.quiz_id
+            console.log(socket.username + "connected on quiz id " + socket.quiz_id);
+            userdata = {
+                username : socket.username,
+                score : 0
+            }
+            saveUser(userdata,socket.quiz_id).then(()=>{
+                io.emit('update_user_list',socket.quiz_id)
+             })         
+        })
                 
+    
+
+        socket.on('disconnect', () => {
+            var newuserList = []
+            usermodel.findOne({quiz_id:socket.quiz_id},(err,response)=>{
+                if(response!=null) 
+                { 
+                    var userList = response.userList
+                
+                    for (let i = 0; i < userList.length; i++) {
+                        var username = userList[i].username
+                       if(username != socket.username)
+                       {
+    
+                        const user ={     
+                            username: username,
+                             score:0
+                        } 
+                        newuserList.push(user)
+                        console.log(newuserList);
+                       }    
+                        
+                    } 
+                    response.userList=newuserList
+                    response.save().then(() => {
+                    io.emit('update_user_list',socket.quiz_id)
+                    console.log(socket.username + " disconnected");
+                    
+                })
+                    
+            }                   
+            })
+        })
+    })
         })
