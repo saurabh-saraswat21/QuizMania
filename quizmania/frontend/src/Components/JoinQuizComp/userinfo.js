57bd5ea5f4df19b4815bf2320413b8afc81df601
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-11-02 09:57:14 +0530

    component complete

--- quizmania/frontend/src/Components/JoinQuizComp/userinfo.js
+++ quizmania/frontend/src/Components/JoinQuizComp/userinfo.js
@@ -7,7 +7,27 @@ function Userinfo(props) {
     const quiz_id = props.match.params.quiz_id
     return (
         <div>
-            
+            <div className="player_info_box">
+                <input type="text" placeholder="enter your nickname" 
+                 onChange={(event)=>{
+                     setusername(event.target.value)
+                     console.log(username);
+                     console.log(quiz_id);
+                 }}
+                
+                />
+
+                <Link  to={{
+
+                    pathname: '/startQuiz/'+quiz_id,
+                    state : {username,quiz_id}
+                  }
+                } >
+
+                <button> Go</button>
+                </Link>
+
+            </div>
         </div>
     )
 }
