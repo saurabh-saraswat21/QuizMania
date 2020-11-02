c1b1d835784760d16e8e809e2b5e8af0e1ec5c8a
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-11-02 09:55:40 +0530

    basic skeleton of the component

--- quizmania/frontend/src/Components/JoinQuizComp/userinfo.js
+++ quizmania/frontend/src/Components/JoinQuizComp/userinfo.js
@@ -1,2 +1,15 @@
 import React ,{useState} from 'react'
 import { Link } from 'react-router-dom'
+
+
+function Userinfo(props) {
+    const [username, setusername] = useState("")
+    const quiz_id = props.match.params.quiz_id
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Userinfo
