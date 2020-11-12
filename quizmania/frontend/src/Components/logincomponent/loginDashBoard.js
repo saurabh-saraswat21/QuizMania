cfd9a4f4f263af36dc63f8ada69b37e0d2d3d11b
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-11-12 22:39:14 +0530

    dashboard of user

--- quizmania/frontend/src/Components/logincomponent/loginDashBoard.js
+++ quizmania/frontend/src/Components/logincomponent/loginDashBoard.js
@@ -1,17 +1,13 @@
 import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
+import JoinQuiz from '../partials/JoinQuiz'
 function LoginDashBoard() {
-    const[quiz_id,setquiz_id]= useState(null);
-    const handleChange=(e)=>{
-        setquiz_id(e.target.value)
-    }
+  
     return (
         <div>
             <Link to ='/createquiz'><button>CreateQuiz</button></Link>
-            <input type="text" placeholder="Enter Quiz ID to Join" onChange={(e)=>handleChange(e)}/>
-            <Link to ={{
-                pathname: '/Quiz/enter_info/'+quiz_id
-            }} ><button disabled={!quiz_id}>JoinQuiz</button></Link>
+            <Link to ='/viewquiz'><button>ViewQuiz</button></Link>
+            <JoinQuiz/>
             
         </div>
     )
