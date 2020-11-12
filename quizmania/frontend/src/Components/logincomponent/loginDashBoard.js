3f08e44f46120877c50ca45f37c66d4693787d5f
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-11-12 16:02:39 +0530

    remove cards

--- quizmania/frontend/src/Components/logincomponent/loginDashBoard.js
+++ quizmania/frontend/src/Components/logincomponent/loginDashBoard.js
@@ -1,27 +1,12 @@
 import React from 'react'
-import CardItem from '../HomePageComp/CardItem';
-
+import {Link} from 'react-router-dom'
 function loginDashBoard() {
     return (
         <div>
-            <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            <CardItem
-              src='/images/img-30.png'
-              text='Create Quiz'
-              label='Exam Time'
-              path='/createquiz'
-            />
-            <CardItem
-              src='/images/img 31.png'
-              text='Join Quiz'
-              label='Be Prepared'
-              path='/JoinQuiz'
-            />
-          </ul>
-        </div>
-      </div>
+
+        <Link to ='/createquiz'><button>createquiz</button></Link>
+        <Link to ='/joinquiz'><button>JoinQuiz</button></Link>
+        
         </div>
     )
 }
