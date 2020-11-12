ef44c99ff61ed97dc722a423db940ebe58aad0d4
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-11-12 16:34:48 +0530

    allow to join directly from dashboard

--- quizmania/frontend/src/Components/logincomponent/loginDashBoard.js
+++ quizmania/frontend/src/Components/logincomponent/loginDashBoard.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
-function loginDashBoard() {
+function LoginDashBoard() {
+    const[quiz_id,setquiz_id]= useState(null);
+    const handleChange=(e)=>{
+        setquiz_id(e.target.value)
+    }
     return (
         <div>
-
-        <Link to ='/createquiz'><button>createquiz</button></Link>
-        <Link to ='/joinquiz'><button>JoinQuiz</button></Link>
-        
+            <Link to ='/createquiz'><button>CreateQuiz</button></Link>
+            <input type="text" placeholder="Enter Quiz ID to Join" onChange={(e)=>handleChange(e)}/>
+            <Link to ={{
+                pathname: '/Quiz/enter_info/'+quiz_id
+            }} ><button disabled={!quiz_id}>JoinQuiz</button></Link>
+            
         </div>
     )
 }
 
-export default loginDashBoard
+export default LoginDashBoard
