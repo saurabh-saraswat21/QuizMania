76b110c009871443a0d7ee85768bce3d7feebba3
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-11-16 21:40:30 +0530

    display thhe number of active users

--- quizmania/frontend/src/Components/hostQuizComponent/HostquizPage.js
+++ quizmania/frontend/src/Components/hostQuizComponent/HostquizPage.js
@@ -4,15 +4,22 @@ const ENDPOINT = "192.168.43.91:80"
 var socket
 const HostquizPage = (props) => {
     const [quizName,setQuizName]= useState(null)
-  
+    const[users,setUsers]= useState(0)
+
+
     useEffect(()=>{
         socket= io(ENDPOINT)
-        // setQuiz(this.props.location.state)
+        socket.on('sent_update',(data)=>{
+            console.log(data);
+            setUsers(data)
+        })
+
        setQuizName(props.location.state.quizName);
     })
 
    const  startquiz=(quiz_id)=>{
          socket.emit('start',(quiz_id))
+         
     }
     
     const quiz_id = props.match.params.quiz_id
@@ -24,6 +31,7 @@ const HostquizPage = (props) => {
 
         <h1>Share this Join Code to instantly join the quiz  {quiz_id}</h1>
             <h1> Start  The QUiz NOW</h1>
+        <h1>current no of users :-{users}</h1>
             <button onClick={()=>startquiz(quiz_id)} >Start</button>
             
         </div>
