d330c5347b4e45e718d5d91144c8ab14762a45fb
ssroyal <saurabh.saraswat_cs18@gla.ac.in>
2020-11-18 23:01:17 +0530

    rendering the userlist

--- quizmania/frontend/src/Components/hostQuizComponent/HostquizPage.js
+++ quizmania/frontend/src/Components/hostQuizComponent/HostquizPage.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect } from 'react'
 import io from 'socket.io-client'
+import axios from 'axios'
 const ENDPOINT = "192.168.43.91:80"
 
 var socket
@@ -70,7 +71,7 @@ const HostquizPage = (props) => {
          
     }
     
-    const quiz_id = props.match.params.quiz_id
+  
   
 
     return (
@@ -79,8 +80,9 @@ const HostquizPage = (props) => {
 
         <h1>Share this Join Code to instantly join the quiz  {quiz_id}</h1>
             <h1> Start  The QUiz NOW</h1>
-        <h1>current no of users :-{users}</h1>
+        <h1>current no of users :-{users.length}</h1>
             <button onClick={()=>startquiz(quiz_id)} >Start</button>
+             {userlist}
             
         </div>
     )
